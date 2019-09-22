3fd6fd6b1b8164c787f280d18541e647afac6401
Valerie Jane <valeriejanethoma@gmail.com>
2019-09-22 18:07:11 -0400

    new assests

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ Product.create!(title: 'Rails, Angular, Postgres, and Bootstrap',
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms</em>
+      <em>Native  Apps, Multiple Platforms</em>
       Answer the question “Can we build this for ALL the devices?” with a
       resounding YES. This book will help you get there with a real-world
       introduction to seven platforms, whether you’re new to mobile or an
